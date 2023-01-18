8b02b9adb01d2abaddce4add1f11aa79e767a73f
Devansh Sharma <sharma.devansh0@gmail.com>
2023-01-18 17:11:26 +0530

    Added expenseitem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>March 28 , 2021</div>
+            <div>
+                <h2>Car Insaurance</h2>
+                <div>249Rs</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem
