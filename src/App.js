dbfe09314e8c26400001050a268be4d7e754c906
Devansh Sharma <sharma.devansh0@gmail.com>
2023-01-25 15:33:41 +0530

    Completed Chart Feature

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
+import React, { useState } from 'react'
 import './index.css'
-import ExpenseItem from './components/ExpenseItem'
+import NewExpense from './components/NewExpense/NewExpense'
 import './components/Expenses.css'
 import Card from './components/Card'
+import ExpensesFilter from './components/Expenses/ExpensesFilter';
+import ExpensesList from './components/Expenses/ExpensesList'
+import ExpensesChart from './components/Expenses/ExpensesChart'
 
-const  App = () => {
-    const expenses = [
-        { id : 'e1' , name : 'New Expense 1' , date : new Date(2021 ,5 ,21) , amount : 100},
-        { id : 'e2' , name : 'New Expense 2' , date : new Date(2022 ,5 ,21) , amount : 200},
-    ]
-    return( 
+const App = () => {
+  const DUMMY_EXPENSES = [
+    { id: 'e1', name: 'New Expense 1', date: new Date(2021, 5, 21), amount: 100 },
+    { id: 'e2', name: 'New Expense 2', date: new Date(2022, 5, 21), amount: 200 },
+  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+  const addExpenseHandler = expense => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    })
+  }
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
+ 
+  return (
     <div>
-        <h1>Hi there!</h1>
-        <Card className='expenses'>
-        <ExpenseItem 
-          title = { expenses[0].name}
-          amount = { expenses[0].amount}
-          date = { expenses[0].date}
-        />
-        <ExpenseItem 
-          title = { expenses[1].name}
-          amount = { expenses[1].amount}
-          date = { expenses[1].date}
-        />
-        </Card>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Card className='expenses'>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses} />
+      </Card>
     </div>
-    );
+  );
 }
 
-  
-  export default App;
+
+export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import React , { useState } from 'react';
 import './ExpenseItem.css' ;
 import ExpenseDate  from './ExpenseDate';
 import Card from './Card';
 
 const ExpenseItem = (props) =>{
+    const [title , setTitle] = useState(props.title);
     const clickHandler = () => {
-        console.log("Clicked");
+        setTitle('Updated !!');
     }
     return (
         <Card className="expense-item">
@@ -12,7 +14,7 @@ const ExpenseItem = (props) =>{
                 date={props.date}
             />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
             <button onClick={clickHandler}>Click Me</button>
